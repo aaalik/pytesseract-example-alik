f10d6190d41567200925494a6eebd2f40c498c5f
alik <anothermalik@gmail.com>
2018-03-27 10:38:33 +0700

    convert using forloop

--- example.py
+++ example.py
@@ -1,11 +1,19 @@
 import pytesseract
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image
+import os, os.path
 
-# im = Image.open("surat.png") # the second one 
-# im = im.filter(ImageFilter.MedianFilter())
-# enhancer = ImageEnhance.Contrast(im)
-# im = enhancer.enhance(2)
-# im = im.convert('1')
-# im.save('surat.png')
-text = pytesseract.image_to_string(Image.open('surat-izin.jpg'), lang='ind')
-print(text)
+# path joining version for other paths
+DIR = 'img'
+countfiles = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+onlyfiles = os.listdir(DIR)
+
+# convert all img files into txt using forloop
+for i in range(countfiles):
+    imgpath = DIR+onlyfiles[i]
+    text = pytesseract.image_to_string(Image.open(imgpath), lang='ind')
+    print(text)
+    print("============================================")
+
+# converting process to txt
+# text = pytesseract.image_to_string(Image.open('img/surat-izin.jpg'), lang='ind')
+# print(text)
